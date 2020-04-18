85e33719e103fa4130489b61551cfc738a7dad1f
linfen <983604047@qq.com>
2020-04-18 14:13:20 +0800

    修复部署后的邮件发送失败

--- linfen-common/src/main/java/cn/linfenw/common/utils/StringEncryptorUtil.java
+++ linfen-common/src/main/java/cn/linfenw/common/utils/StringEncryptorUtil.java
@@ -9,7 +9,7 @@ public class StringEncryptorUtil {
         //加密所需的salt(盐)
         textEncryptor.setPassword("EbfYkitulv73I2p0mXI50JMXoaxZTKJ1");
         //要加密的数据（数据库的用户名或密码）
-        String username = textEncryptor.encrypt("983604047@qq.com");
+        String username = textEncryptor.encrypt("linfenw@qq.com");
         String password = textEncryptor.encrypt("lxdqwicdr-kaiwanxiao");
         System.out.println("username:"+username);
         System.out.println("password:"+password);
